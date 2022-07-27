5c7ad04b84a68378b4fb72b6313cf92731791536
panchuk <panchuk6@gmail.com>
2022-07-27 16:36:41 +0300

    App init

---
+++ main.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const request = require('request');
+const http = require('http');
+const port = process.argv.slice(2)[0];
+const app = express();
+
+app.use(bodyParser.json());
+
+app.get('/', async (req, res) => {
+    res.end(`Hello UserName`);
+});
+http.createServer(app).listen(80);
