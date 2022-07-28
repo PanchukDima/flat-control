63308a403b983ed424eacd235ae32889f97eeb55
panchuk <panchuk6@gmail.com>
2022-07-28 13:49:16 +0300

    add ui login form

--- main.js
+++ main.js
@@ -33,7 +33,7 @@ mongoClient.connect(function(err, client){
 });
 
 app.get('/api/auth/', async (req, res) => {
-    res.render('Pages/login.html');
+    res.redirect(302, 'index.html');
 });
 
 app.post('/api/auth/', async (req, res) =>{
