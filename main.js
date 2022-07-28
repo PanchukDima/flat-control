9307ed4d70148eff45a160ebd787f888b9c43915
panchuk <panchuk6@gmail.com>
2022-07-28 09:57:14 +0300

    mongo db add connect

--- main.js
+++ main.js
@@ -3,10 +3,21 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const request = require('request');
 const http = require('http');
+const MongoClient = require("mongodb").MongoClient;
 const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(bodyParser.json());
+
+const mongoClient = new MongoClient(ENV['mongo_str']);
+mongoClient.connect(function(err, client){
+
+    if(err){
+        return console.log(err);
+    }
+    // взаимодействие с базой данных
+    client.close();
+});
 
 app.get('/api/auth/', async (req, res) => {
     res.end(`Hello UserName`);
