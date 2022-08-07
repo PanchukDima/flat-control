42f8724cc225194af3dc8828f4e040da03865cf0
panch-dima(Home) <panchuk6@gmail.com>
2022-08-07 22:25:58 +0300

    test

--- main.js
+++ main.js
@@ -61,7 +61,7 @@ app.post('/static/login.html', urlencodedParser,function (req, res) {
                 code: tmp_key,
                 client_id: process.env.clientkey,
             };*/
-            console.log( req.query.redirect_uri+'?state='+req.query.state+'&code='+tmp_key+'&client_id'+process.env.clientkey);
+            console.log(req.query.redirect_uri+encodeURI('?state='+req.query.state+'&code='+tmp_key+'&client_id'+process.env.clientkey));
             //res.redirect(302, req.query.redirect_uri+'?state='+req.query.state+'&code='+tmp_key+'&client_id'+process.env.clientkey);
         }
     });
