841bed6ae51fff596e76c906476006e21c217ff2
panch-dima(Home) <panchuk6@gmail.com>
2022-08-08 15:35:01 +0300

    Oauth testing fuck

--- main.js
+++ main.js
@@ -131,10 +131,14 @@ app.get('/v1.0/user/devices', urlencodedParser,(req, res) => {
         let authorization = req.headers.authorization;
         let TokenArray = authorization.split(" ");
         console.log(TokenArray[1]);
-        let userData = Client.findOne({oauth:{key:TokenArray[1]}});
-        console.log(JSON.stringify(userData));
+        Client.findOne({oauth:{key:TokenArray[1]}},function(err,result)
+        {
+            console.log(JSON.stringify(result));
+            res.end('get devices list user');
+        });
+
     });
-    res.end('get devices list user');
+
 });
 
 app.post('/v1.0/user/devices/query', (req, res) => {
