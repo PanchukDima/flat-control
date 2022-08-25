26af4d1f9bcf5f8e9adbbf5a9ad0eded2e2cb46f
panchuk <panchuk6@gmail.com>
2022-08-25 13:16:35 +0300

    Tcp_server/add connect function delete old method

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ server.on('connection', function(sock) {
     console.log('CONNECTED: ' + sock.remoteAddress + ':' + sock.remotePort);
     sockets.push(sock);
     sock.on('data', function(data) {
-        console.log('DATA ' + sock.remoteAddress + ': ' + data);
+        console.log('DATA ' + sock.remoteAddress + ': ' + data[0]);
 // Write the data back to all the connected, the client will receive it as data from the server
         sockets.forEach(function(sock, index, array) {
             sock.write(sock.remoteAddress + ':' + sock.remotePort + " said " + data + '\n');
