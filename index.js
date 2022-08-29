f2ba53608d610c730dfef3bce867e73aea6e190f
panchuk <panchuk6@gmail.com>
2022-08-29 10:52:59 +0300

    Tcp_server/control devices function change state device port

--- index.js
+++ index.js
@@ -143,7 +143,7 @@ app.post('/api/sendtoken/', function (req, res){
     if(!req.body) {
         return res.sendStatus(400);
     }
-    console.log(req.query);
+    console.log(req);
     console.log(req.query['redirect_uri']/*+'?state='+req.query.state+'&code='+tmp_key+'&client_id='+process.env.clientkey*/);
     /*mongoClient.connect(function(err, client) {
 
