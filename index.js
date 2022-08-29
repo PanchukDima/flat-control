eb49244f0c073b839105264579c3b56fb52ef9fd
panchuk <panchuk6@gmail.com>
2022-08-29 11:23:03 +0300

    Tcp_server/control devices function change state device port

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const request = require('request');
 const http = require('http');
 var uuid = require('uuid');
 var ObjectId = require('mongodb').ObjectId;
-var url = require('url');
+
 
 
 const net = require('net');
@@ -146,7 +146,7 @@ app.post('/api/sendtoken/', urlencodedParser, function (req, res){
         return res.sendStatus(400);
     }
     console.log(req.rawHeaders['Referer']);
-    var queryData = url.parse(req.rawHeaders['Referer'], true).query;
+
     console.log(queryData);
     //?state=https://social.yandex.ru/broker2/authz_in_web/0f03516f2dbe43428c3b2700e10f7253/callback&redirect_uri=https://social.yandex.net/broker/redirect&response_type=code&client_id=cfe4413eeb8f4a30ba5e7c7b9a777e04
     console.log(req.query['redirect_uri']/*+'?state='+req.query.state+'&code='+tmp_key+'&client_id='+process.env.clientkey*/);
