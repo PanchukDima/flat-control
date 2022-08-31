a8c7ff16f5752c092cd88af32896f456dd26d318
panchuk <panchuk6@gmail.com>
2022-08-31 17:57:33 +0300

    hh

--- index.js
+++ index.js
@@ -353,7 +353,7 @@ app.post('/v1.0/user/devices/action', urlencodedParser, (req, res) => {
                     "devices.$.capabilities": devices[0].capabilities
                 }
             }, function (err, result) {
-                console.log("Update result" + result);
+                console.log("Update result" + JSON.stringify(result));
                 responseBody.payload.devices = result;
                 /*let device = sockets.find(devices => devices.id === req.body.payload.devices[0].id);
                 device.net_sock.write("20:"+req.body.payload.devices[0].id+"0:255");*/
