3e267c94a44249df2c91a3b1defb0f4e086d3647
panchuk <panchuk6@gmail.com>
2022-09-01 10:04:59 +0300

    Tcp_server/control devices function change state device port

--- index.js
+++ index.js
@@ -347,8 +347,7 @@ app.post('/v1.0/user/devices/action', urlencodedParser, (req, res) => {
                     "devices.$.capabilities": devices[0].capabilities
                 }
             }, function (err, result) {
-                console.log("Update result" + JSON.stringify(result));
-                responseBody.payload.devices = result.value.action_result = {"status":"DONE"};
+
                 Client.find({
                     oauth: {
                         key: TokenArray[1]
@@ -362,9 +361,11 @@ app.post('/v1.0/user/devices/action', urlencodedParser, (req, res) => {
                     if (err) {
                         throw err
                     }
+                    responseBody.payload.devices = result[0].devices;
+                    responseBody.payload.devices[0].action_result = {"status":"DONE"};
                     /*let device = sockets.find(devices => devices.id === req.body.payload.devices[0].id);
                     device.net_sock.write("20:"+req.body.payload.devices[0].id+"0:255");*/
-                    console.log(JSON.stringify(result, null, 3));
+                    console.log(JSON.stringify(responseBody, null, 3));
                     res.setHeader('Content-Type', 'application/json');
                     res.end(JSON.stringify(responseBody, null, 3));
                 });
