53104f6f6ee34d140193f6c5e07658d0c61d5126
panchuk <panchuk6@gmail.com>
2023-09-11 10:13:37 +0300

    add support mqtt from nodejs

--- index.js
+++ index.js
@@ -289,15 +289,7 @@ app.post('/v1.0/user/devices/action', urlencodedParser, (req, res) => {
                 }
                 responseBody.payload.devices = dbres.rows[0].json_agg;
                 dbres.rows[0].mqtt.forEach((element) => {
-                    client_mqtt.publish(element.topic, element.down, {qos: 2}, (err) => {
-                        if (err) {
-                            console.error('Failed to publish message:', err);
-                        } else {
-                            console.log('Message published with retain flag set to true');
-                        }
-                    });
-
-                    client_mqtt.publish(element.topic, element.up, {qos: 2}, (err) => {
+                    client_mqtt.publish(element.topic, element.message, {qos: 2}, (err) => {
                         if (err) {
                             console.error('Failed to publish message:', err);
                         } else {
