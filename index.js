7a66b280c6dda435cd158741fa12f70e85859959
panchuk <panchuk6@gmail.com>
2023-09-12 14:18:31 +0300

    add support mqtt from nodejs

--- index.js
+++ index.js
@@ -299,7 +299,7 @@ app.post('/v1.0/user/devices/action', urlencodedParser, (req, res) => {
                         });
                     }, 500 * i, i);
                 }
-            });
+
                 res.setHeader('Content-Type', 'application/json');
                 res.end(JSON.stringify(responseBody, null, 3));
             }
